09d1c860de34d0dada01126dfdbece8c16ef76fd
Raj Aryan <143009186+aryanraj45@users.noreply.github.com>
2025-10-01 23:29:38 +0530

    Update RotateString.java

---
+++ 0796-Rotate-String/RotateString.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n=s.length();
+        int m=goal.length();
+        String s1= new String();
+
+        if(n!=m)
+        {
+         return false;
+        }
+        for(int i=0;i<n;i++)
+        {
+            s1=s.substring(i) +s.substring(0,i);
+            if(s1.equals(goal))
+            {
+                return true;
+            }
+        }
+            return false;
+    }
+}
