26e1f77187d541c9d0bac19a3825adf096b4de45
Raj Aryan <143009186+aryanraj45@users.noreply.github.com>
2025-10-01 22:47:05 +0530

    Update FindAPeakElementII.java

--- 1901-Find-a-Peak-Element-II/FindAPeakElementII.java
+++ 1901-Find-a-Peak-Element-II/FindAPeakElementII.java
@@ -1,2 +1,30 @@
-git add 2149-Rearrange-Array-Elements-by-Sign/
-git commit -m "feat: Add solution for 2149. Rearrange Array Elements by Sign"
+class Solution {
+    public int[] findPeakGrid(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int left = 0, right = n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            // find max row in this column
+            int maxRow = 0;
+            for (int i = 0; i < m; i++) {
+                if (mat[i][mid] > mat[maxRow][mid]) {
+                    maxRow = i;
+                }
+            }
+
+            boolean leftBigger = (mid > 0 && mat[maxRow][mid-1] > mat[maxRow][mid]);
+            boolean rightBigger = (mid < n-1 && mat[maxRow][mid+1] > mat[maxRow][mid]);
+
+            if (!leftBigger && !rightBigger) {
+                return new int[]{maxRow, mid}; // found peak
+            } else if (rightBigger) {
+                left = mid + 1;  // move right
+            } else {
+                right = mid - 1; // move left
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
