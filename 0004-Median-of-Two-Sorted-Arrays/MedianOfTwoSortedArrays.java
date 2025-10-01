24f74e0ecf411cbcf221fffd664dc9404af82a69
Raj Aryan <143009186+aryanraj45@users.noreply.github.com>
2025-10-01 22:57:48 +0530

    Update MedianOfTwoSortedArrays.java

--- 0004-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.java
+++ 0004-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        
+        // Merge two arrays
+        int[] merged = new int[n + m];
+        System.arraycopy(nums1, 0, merged, 0, n);
+        System.arraycopy(nums2, 0, merged, n, m);
+        
+        Arrays.sort(merged);
+        
+        int len = merged.length;
+        
+        if (len % 2 == 0) {
+            return (merged[len/2 - 1] + merged[len/2]) / 2.0;
+        } 
+        
+        else {
+            return merged[len/2];
+        }
+    }
+}
 
 
