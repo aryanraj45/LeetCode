dceed1518fa9ef21e66e5d5dc241f93f4f5cbeac
aryanraj45 <rajaryan445567@gmail.com>
2026-01-23 19:21:45 +0530

    Small change 2 in MedianOfTwoSortedArrays.java

--- 0004-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.java
+++ 0004-Median-of-Two-Sorted-Arrays/MedianOfTwoSortedArrays.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 class Solution {
+    // Small change 2
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
